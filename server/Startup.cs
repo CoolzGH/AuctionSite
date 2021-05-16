99d5ad18ec1e7558f9785105252d7d0b100463c3
Maksim Zorin <zoromaxim@yandex.ru>
2021-05-16 19:09:20 +0300

    dockerbuild

--- server/Startup.cs
+++ server/Startup.cs
@@ -51,10 +51,10 @@ namespace Server
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
-            
+
             app.UseStaticFiles();   // добавляем поддержку статических файлов
 
             app.UseRouting();
